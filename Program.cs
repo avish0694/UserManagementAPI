e8860b82648393257e229f3db600c3d3a38d1aeb
Avish Cheetaram <avishcheetaram.ai@gmail.com>
2025-03-21 15:22:11 +0400

    init

---
+++ Program.cs
@@ -0,0 +1,73 @@
+using System.ComponentModel.DataAnnotations;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var users = new List<User>
+{
+    new User { Id = 1, Name = "Alice", Email = "alice@example.com" },
+    new User { Id = 2, Name = "Bob", Email = "bob@example.com" }
+};
+
+app.MapGet("/users", () => Results.Ok(users));
+
+// Get User by id
+app.MapGet("/users/{id}", (int id) =>
+{
+    var user = users.FirstOrDefault(u => u.Id == id);
+    return user is not null ? Results.Ok(user) : Results.NotFound();
+});
+
+// Add a user
+app.MapPost("/users", (User user) =>
+{
+    user.Id = users.Count > 0 ? users.Max(u => u.Id) + 1 : 1;
+    users.Add(user);
+    return Results.Created($"/users/{user.Id}", user);
+});
+
+// Update a user
+
+app.MapPut("/users/{id}", (int id, User updatedUser) =>
+{
+    var user = users.FirstOrDefault(u => u.Id == id);
+    if (user is null) return Results.NotFound();
+
+    user.Name = updatedUser.Name;
+    user.Email = updatedUser.Email;
+    return Results.Ok(user);
+});
+
+// Delete a user
+
+app.MapDelete("/users/{id}", (int id) =>
+{
+    var user = users.FirstOrDefault(u => u.Id == id);
+    if (user is null) return Results.NotFound();
+
+    users.Remove(user);
+    return Results.NoContent();
+});
+
+app.Run();
+
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+}
